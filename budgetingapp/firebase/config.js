cc1a06032fc05b25698af07a08fd1b6e68d19143
Tatu Lehtola <32382567+Tappivanukas2@users.noreply.github.com>
2025-03-18 01:47:01 +0200

    Update config.js

--- budgetingapp/firebase/config.js
+++ budgetingapp/firebase/config.js
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 //process.env.REACT_APP_API_KEY
 const firebaseConfig = {
-    apiKey: "AIzaSyBFyBpPXzdPiT-OEHGE3zTMlAdGn9rAaw0",
+    apiKey: "",
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
