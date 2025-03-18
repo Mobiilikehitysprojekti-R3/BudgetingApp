ec03860eee3dc231a543e7eb17a7ae90e3f145bc
Tatu Lehtola <32382567+Tappivanukas2@users.noreply.github.com>
2025-03-18 13:42:15 +0200

    Budget and income functions updating

--- budgetingapp/firebase/firestore.js
+++ budgetingapp/firebase/firestore.js
@@ -5,19 +5,51 @@ import { getDoc } from "firebase/firestore";
 
 const db = getFirestore();
 const auth = getAuth();
-// Save income to user data in Firestore
-async function saveUserData(income) {
-    if (auth.currentUser) {
-        const userRef = doc(db, "users", auth.currentUser.uid);
-        await setDoc(userRef, { income: income }, { merge: true });
-        console.log("Income added to user data.");
+
+// Get the currently signed-in user
+const userId = auth.currentUser;
+
+// Function to create an income field and update it to Firestore
+const updateUserIncome = async (userId, income) => {
+    if (!userId) {
+      console.error("No user logged in.");
+      return;
     } else {
-        console.log("No user signed in.");
+        try {
+            await updateDoc(doc(db, "users", userId), {
+                income: income, // Add or update the "income" field
+            });
+            console.log("Income field added/updated!");
+            } catch (error) {
+            console.error("Error updating user data:", error);
+            }
     }
-}
+  };
 
-saveUserData(50000);
+// Function to create a budget field and update it to Firestore
+const updateUserBudget = async (userId, budget) => {
+    if (!userId) {
+        console.error("No user logged in.");
+        return;
+    } else {
+        try {
+            await updateDoc(doc(db, "users", userId), {
+                budget: budget, // Add or update the "budget" field
+            });
+            console.log("Budget field added/updated!");
+            } catch (error) {
+            console.error("Error updating user data:", error);
+            }
+    }
+};
+  
+// Test values for income and budget
+updateUserIncome(userId, 50000);
+updateUserBudget(userId, 10000);
 
+
+// Function to get user data from Firestore
+// This function will log the user data to the console
 async function getUserData() {
     if (auth.currentUser) {
         const userRef = doc(db, "users", auth.currentUser.uid);
