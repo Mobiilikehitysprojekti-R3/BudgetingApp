fb97a3e5f3c0f17123cf5f9a3a5eff21a5670a1d
nidacnr <nida.cinar23@gmail.com>
2025-03-24 11:43:09 +0200

    update
    MyGroups ja Group sivut

--- budgetingapp/pages/NoGroups.js
+++ budgetingapp/pages/NoGroups.js
@@ -2,34 +2,21 @@ import React, { useState } from 'react';
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import styles from '../styles';
 
-export default function NoGroups ({ navigation }) {
+export default function NoGroups({ navigation }) {
     const [groups] = useState([]); 
   
     const handleCreateGroup = () => {
-      navigation.navigate('CreateGroup'); // Navigate to CreateGroup page
-    };
+        navigation.navigate('CreateGroup'); 
   
     return (
-      <View style={styles.container}>
-          <Text style={styles.title}>MY GROUPS</Text>
-        {groups.length === 0 ? (
-          <View style={styles.emptyState}>
-            <Text style={styles.emptyText}>You're not in any group yet, start here</Text>
-            <TouchableOpacity style={styles.buttonTwo} onPress={handleCreateGroup}>
-              <Text style={styles.buttonText}>Create Group</Text>
-            </TouchableOpacity>
-          </View>
-        ) : (
-          <FlatList
-            data={groups}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <View style={styles.groupItem}>
-                <Text style={styles.groupName}>{item.name}</Text>
-              </View>
-            )}
-          />
-        )}
-      </View>
-    );
-  };
+        <View style={styles.container}>
+            <Text style={styles.title}>MY GROUPS</Text>
+            <View >
+                <Text >You're not in any group yet, start here</Text>
+                <TouchableOpacity style={styles.buttonTwo} onPress={handleCreateGroup}>
+                    <Text style={styles.buttonText}>Create Group</Text>
+                </TouchableOpacity>
+            </View>
+        </View> 
+    ); 
+}}
