70c47369d23d153e909ffbf7cd6e6aca8d9ffd84
KarolusPaulus <karosaarinenn@gmail.com>
2025-04-13 15:21:52 +0300

    Update GroupBudget.js

--- budgetingapp/pages/GroupBudget.js
+++ budgetingapp/pages/GroupBudget.js
@@ -140,7 +140,7 @@ export default function GroupBudget({ route, navigation }) {
 
   const handleSlicePress = (category) => {
     setActiveCategory(category)
-  setDetailModalVisible(true)
+    setDetailModalVisible(true)
   }
 
   useEffect(() => {
